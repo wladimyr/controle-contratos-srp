7a56ed490df43576bf2966868e0e212373cb94d7
Wladimyr Garcia Mançano <39243274+wladimyr@users.noreply.github.com>
2026-08-02 05:24:40 -0300

    salvando alteracoes locais

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,14 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
+<<<<<<< HEAD
 const supabaseUrl = 'sb_publishable_r_sssaepolvrfafqttrvth'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzc2FlcG9sdnJmYWZxdHRydnRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU2NDE1MzUsImV4cCI6MjEwMTIxNzUzNX0.pabLfKw9-d2zHsLxl4XWCRk_fM001BEzqH2X5tYamag'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+=======
+// Tenta pegar do ambiente (.env); se não encontrar, usa a URL/Key direta
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://controle-contratos-srp.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzc2FlcG9sdnJmYWZxdHRydnRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU2NDE1MzUsImV4cCI6MjEwMTIxNzUzNX0.pabLfKw9-d2zHsLxl4XWCRk_fM001BEzqH2X5tYamag';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+>>>>>>> 27931b8 (atualizando codigo e layout)
