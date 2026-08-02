b923fed08c561831a01a9a2e775ca6cec9530a0d
Wladimyr Garcia Mançano <39243274+wladimyr@users.noreply.github.com>
2026-08-02 03:48:25 -0300

    atualizando codigo e layout

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/controle-contratos-srp/', // Substitua pelo nome exato do seu repositório no GitHub
 })
