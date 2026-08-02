894c9431572b0f0fe10294981bba6aeafdf95d76
Wladimyr Garcia Mançano <39243274+wladimyr@users.noreply.github.com>
2026-08-02 20:22:04 -0300

    atualizando codigo e layout

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -2,7 +2,6 @@
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://sssaepolvrfafqttrvth.supabase.co'
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzc2FlcG9sdnJmYWZxdHRydnRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU2NDE1MzUsImV4cCI6MjEwMTIxNzUzNX0.pabLfKw9-d2zHsLxl4XWCRk_fM001BEzqH2X5tYamag';
-const supabase = createClient(supabaseUrl, supabaseKey)
 
-/ Adicionado a palavra 'export' para que o App.tsx consiga importar
+// Criado apenas UMA vez com o export
 export const supabase = createClient(supabaseUrl, supabaseKey);
