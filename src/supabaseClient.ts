c28d6fc3f6586a6e3bcdd8ff33ae591b5d94a4d5
Wladimyr Garcia Mançano <39243274+wladimyr@users.noreply.github.com>
2026-08-02 05:37:44 -0300

    atualizando codigo e layout

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,5 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://sssaepolvrfafqttrvth.supabase.co'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://sssaepolvrfafqttrvth.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
+
+/ Adicionado a palavra 'export' para que o App.tsx consiga importar
+export const supabase = createClient(supabaseUrl, supabaseKey);
